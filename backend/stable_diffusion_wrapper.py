7ecdf981a913e988724e85b8d5fbdefa1bf0ffdb
Yash Gohel <yashgohel16@gmail.com>
2022-12-19 16:40:37 +0530

    s2

--- backend/stable_diffusion_wrapper.py
+++ backend/stable_diffusion_wrapper.py
@@ -3,7 +3,7 @@ import torch
 
 class StableDiffusionWrapper:
     def __init__(self) -> None:
-        repo_id = "stabilityai/stable-diffusion-2-base"
+        repo_id = "stabilityai/stable-diffusion-2"
         pipe = DiffusionPipeline.from_pretrained(
             repo_id, revision="fp16",
             torch_dtype=torch.float16
