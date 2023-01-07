43918ad6a1d3d302b25fe4601bb2b532a5357aeb
Yash Gohel <yashgohel16@gmail.com>
2023-01-07 16:24:13 +0530

    enhance

--- backend/stable_diffusion_wrapper.py
+++ backend/stable_diffusion_wrapper.py
@@ -1,9 +1,12 @@
-from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
-import torch
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler, StableDiffusionUpscalePipelin
+import requests
+from PIL import Image
+from io import BytesIO
 
 class StableDiffusionWrapper:
-    def __init__(self, model = "stabilityai/stable-diffusion-2") -> None:
-        repo_id = model
+    def __init__(self, model = "stabilityai/stable-diffusion-2-1-base") -> None:
+        self.repo_id = model
+
         # repo_id = "stabilityai/stable-diffusion-2"
         # repo_id = "stabilityai/stable-diffusion-2-depth"
         # pipe = DiffusionPipeline.from_pretrained(
@@ -11,17 +14,33 @@ class StableDiffusionWrapper:
         #     torch_dtype=torch.float16
         # )
 
-        pipe = DiffusionPipeline.from_pretrained(
-            repo_id,
-            torch_dtype=torch.float16
-        )
+        if (self.model_id != "stabilityai/stable-diffusion-x4-upscaler"):
+            pipe = DiffusionPipeline.from_pretrained(
+                self.repo_id,
+                torch_dtype=torch.float16
+            )
 
+            pipe.scheduler = DPMSolverMultistepScheduler.from_config(
+                pipe.scheduler.config)
+            self.pipe = pipe.to("cuda")
+
+    
+    def enhance_image(self, text_prompt: str, img: str, steps: int = 5):
+        self.model_id = "stabilityai/stable-diffusion-x4-upscaler"
+        pipe = StableDiffusionUpscalePipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
         pipe.scheduler = DPMSolverMultistepScheduler.from_config(
             pipe.scheduler.config)
         self.pipe = pipe.to("cuda")
 
-            
-    def generate_images(self, text_prompt: str, num_images: int, steps: int = 20):
+        response = requests.get(img)
+        low_res_img = Image.open(BytesIO(response.content)).convert("RGB")
+        low_res_img = low_res_img.resize((128, 128))
+
+        upscaled_image = self.pipe(prompt=prompt, image=low_res_img, num_inference_steps=steps).images[0]
+        return upscaled_image
+
+
+    def generate_images(self, text_prompt: str, num_images: int, steps: int = 5):
         prompt = [text_prompt] * num_images
         images = self.pipe(prompt, num_inference_steps=steps).images
         return images
